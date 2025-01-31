e27edd72fd0fb7807ac728c82d4e07fcbf249a1d
Lê Minh Trí <leminhtri11a1@gmail.com>
2025-01-31 15:17:00 +0700

    first commit

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+    plugins: {
+        "postcss-import": {},
+        "tailwindcss/nesting": {},
+        tailwindcss: {},
+        autoprefixer: {},
+        ...(process.env.NODE_ENV === "production"
+            ? {
+                  cssnano: {
+                      preset: ["default", { discardComments: { removeAll: true } }],
+                  },
+              }
+            : {}),
+    },
 };
 
 export default config;
