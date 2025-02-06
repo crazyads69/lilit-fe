5807b900a8c3e7cf1302b3e5c394f7a7521223ed
Lê Minh Trí <leminhtri11a1@gmail.com>
2025-02-06 21:33:13 +0700

    feat: Integrate Redux Toolkit and store
    This commit integrates Redux Toolkit for state management and creates a basic store.  The ReduxProvider component wraps the application to provide Redux context.  Several SVG files and the manifest.json file were removed as they were not in use.
    
    Authored-by: Lê Minh Trí <leminhtri11a1@gmail.com>

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
+import ReduxProvider from "@/redux/provider/provider";
 import MaterialThemeProvider from "@/theme/theme-provider";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return <MaterialThemeProvider>{children}</MaterialThemeProvider>;
+    return (
+        <ReduxProvider>
+            <MaterialThemeProvider>{children}</MaterialThemeProvider>
+        </ReduxProvider>
+    );
 }
